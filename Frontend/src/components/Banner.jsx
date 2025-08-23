44623f6d5796386260db25a9d5f610cf1a6d749e
godlevel-infinite <jstormbraker07@gmail.com>
2025-08-23 23:48:04 +0530

    front-1

---
+++ Frontend/src/components/Banner.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import banner from "/Banner.png";
+function Banner() {
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-10">
+
+        <div className="order-2 md:order-1 w-full md:w-1/2 mt-12 md:mt-36">
+          <div className="space-y-8">
+            <h1 className="text-2xl md:text-4xl font-bold">
+              Hello, welcomes here to learn something{" "}
+              <span className="text-pink-500">new everyday!!!</span>
+            </h1>
+            <p className="text-sm md:text-xl">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+              et totam. Tempora amet atque expedita, quae corrupti totam sed
+              pariatur corporis at veniam est voluptas animi!
+            </p>
+            <label className="input validator w-full">
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                </g>
+              </svg>
+              <input type="email" placeholder="mail@site.com" required />
+            </label>
+            <div className="validator-hint hidden">
+              Enter valid email address
+            </div>
+          </div>
+          <button className="btn mt-6 btn-secondary">Secondary</button>
+        </div>
+
+        {/* Right Section */}
+        <div className=" order-1 w-full md:w-1/2 mt-12 md:mt-36">
+        <img src={banner} className="w-92 h-92" alt="" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Banner;
